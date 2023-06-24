d2953b2a00f60c9d9e22b0f1660ab6b42de0e230
chixx <lucottejeremie@gmail.com>
2023-06-24 08:27:59 +0200

    init Hoodlrz contract add ERC721A and start contract

--- src/Hoodlrz.sol
+++ src/Hoodlrz.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Hoodlrz {
-  
+import "lib/ERC721A/contracts/ERC721A.sol";
+
+contract Hoodlrz is ERC721A {
+  uint256 maxSupply = 1000;
+
+  constructor() ERC721A("Hoodlrz", "HDZ") {}
+
+  function setMaxSupply(uint256 newMaxSupply) external {}
 }
