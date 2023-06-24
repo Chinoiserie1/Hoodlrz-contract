ae4e2bcf3da6feba8749940792eff2866a623ee5
chixx <lucottejeremie@gmail.com>
2023-06-24 08:40:41 +0200

    upgrade Hoodlrz contract add setMaxSupply

--- src/Hoodlrz.sol
+++ src/Hoodlrz.sol
@@ -4,10 +4,22 @@ pragma solidity ^0.8.13;
 import "ERC721A/ERC721A.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Hoodlrz is ERC721A {
+error contractFreezed();
+error currentSupplyExceedNewMaxSupply();
+
+contract Hoodlrz is ERC721A, Ownable {
   uint256 maxSupply = 1000;
+
+  bool freezeContract;
+
+  event SetNewMaxSupply(uint256 newMaxSupply);
 
   constructor() ERC721A("Hoodlrz", "HDZ") {}
 
-  function setMaxSupply(uint256 newMaxSupply) external {}
+  function setMaxSupply(uint256 newMaxSupply) external onlyOwner {
+    if (freezeContract) revert contractFreezed();
+    if (totalSupply() < newMaxSupply) revert currentSupplyExceedNewMaxSupply();
+    maxSupply = newMaxSupply;
+    emit SetNewMaxSupply(newMaxSupply);
+  }
 }
