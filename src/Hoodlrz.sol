5c7cdc146cb0c84675ea7267f515de9c954c17d5
chixx <lucottejeremie@gmail.com>
2023-06-24 09:38:13 +0200

    upgrade Hoodlrz contract add setBaseURI

--- src/Hoodlrz.sol
+++ src/Hoodlrz.sol
@@ -14,6 +14,7 @@ contract Hoodlrz is ERC721A, Ownable {
   bool public freezeContract;
 
   event SetNewMaxSupply(uint256 newMaxSupply);
+  event SetNewBaseURI(string newBaseURI);
   event FreezeContract();
 
   constructor() ERC721A("Hoodlrz", "HDZ") {}
@@ -22,15 +23,25 @@ contract Hoodlrz is ERC721A, Ownable {
 
   /**
    * @notice Set max supply that can be minted
-   * @param newMaxSupply the new max supply
+   * @param _newMaxSupply the new max supply
    * @dev to call this function newMaxSupply need to be superior that actual supply
    * and the contract not freeze
    */
-  function setMaxSupply(uint256 newMaxSupply) external onlyOwner {
+  function setMaxSupply(uint256 _newMaxSupply) external onlyOwner {
     if (freezeContract) revert contractFreezed();
-    if (totalSupply() < newMaxSupply) revert currentSupplyExceedNewMaxSupply();
-    maxSupply = newMaxSupply;
-    emit SetNewMaxSupply(newMaxSupply);
+    if (totalSupply() < _newMaxSupply) revert currentSupplyExceedNewMaxSupply();
+    maxSupply = _newMaxSupply;
+    emit SetNewMaxSupply(_newMaxSupply);
+  }
+
+  /**
+   * @notice set the new base URI
+   * @param _newBaseURI the new base URI
+   */
+  function setBaseURI(string memory _newBaseURI) external onlyOwner {
+    if (freezeContract) revert contractFreezed();
+    baseURI = _newBaseURI;
+    emit SetNewBaseURI(_newBaseURI);
   }
 
   /**
