51b4354bd75557b62a6d04575b1bfa4fa47baa51
chixx <lucottejeremie@gmail.com>
2023-06-24 08:34:07 +0200

    init Hoodlrz contract add openzeppelin

--- src/Hoodlrz.sol
+++ src/Hoodlrz.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "lib/ERC721A/contracts/ERC721A.sol";
+import "ERC721A/ERC721A.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract Hoodlrz is ERC721A {
   uint256 maxSupply = 1000;
